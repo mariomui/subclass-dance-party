56f43afb1557513c3fdebd230df2d6afa96d8b45
mariomui <yi.kan.mui@gmail.com>
2018-11-12 16:25:21 -0800

    Second Commit

--- src/dancer.js
+++ src/dancer.js
@@ -1,18 +1,30 @@
 var makeDancer = function (top, left, timeBetweenSteps) {
   this.$node = $('<span class="dancer"></span>');
-  this.timeBetweenSteps = timeBetweenSteps;
+  this.step = function () {
+    setTimeout(this.step, timeBetweenSteps);
+  }
+
+  this.setPosition = function (top, left) {
+    var styleSettings = {
+      top: top,
+      left: left
+    };
+    this.$node.css(styleSettings);
+  }
+
   this.step();
   this.setPosition(top, left);
+  // this.setPosition(top, left);
 }
 
-makeDancer.prototype.step = function () {
-  setTimeout(this.step, this.timeBetweenSteps);
-};
 
-makeDancer.prototype.setPosition = function (top, left) {
-  var styleSettings = {
-    top: top,
-    left: left
-  };
-  this.$node.css(styleSettings);
-};
+// makeDancer.prototype.step = function () {
+//   setTimeout(this.step, this.timeBetweenSteps);
+// };
+// makeDancer.prototype.setPosition = function (top, left) {
+//   var styleSettings = {
+//     top: top,
+//     left: left
+//   };
+//   this.$node.css(styleSettings);
+// };
--- src/init.js
+++ src/init.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
   window.dancers = [];
 
-  $('.addDancerButton').on('click', function(event) {
+  $('.addDancerButton').on('click', function (event) {
     /* This function sets up the click handlers for the create-dancer
      * buttons on dancefloor.html. You should only need to make one small change to it.
      * As long as the "data-dancer-maker-function-name" attribute of a
@@ -22,11 +22,12 @@ $(document).ready(function() {
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+    // debugger;
     $('body').append(dancer.$node);
   });
 });
