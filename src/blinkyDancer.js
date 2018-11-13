008c6360deec893317911c53e33b00f354491ff6
mariomui <yi.kan.mui@gmail.com>
2018-11-12 18:25:56 -0800

    Complete Refactoring of DanceParty to Pseudoclassical
    In this commit, we refactored to Psuedo

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,23 +1,20 @@
-// var makeBlinkyDancer = function (top, left, timeBetweenSteps) {
-//   var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
-//   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-//   // so we must keep a copy of the old version of this function
-
-//   var oldStep = blinkyDancer.step;
-
-//   blinkyDancer.step = function () {
-//     // call the old version of step at the beginning of any call to this new version of step
-//     oldStep();
-//     // toggle() is a jQuery method to show/hide the <span> tag.
-//     // See http://api.jquery.com/category/effects/ for this and
-//     // other effects you can use on a jQuery-wrapped html tag.
-//     blinkyDancer.$node.toggle();
-//   };
-
-//   return blinkyDancer;
-// };
-
 var makeBlinkyDancer = function (top, left, timeBetweenSteps) {
   makeDancer.call(this, ...arguments);
+  // this.$node = $('<span class="dancer"></span>');
+  this.setPosition(top, left);
 }
+
+makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+
+makeBlinkyDancer.prototype.step = function () {
+  // Refer to superclass .step
+  // while it's timing out, call toggle
+
+  makeDancer.prototype.step.call(this);
+  this.$node.toggle();
+}
+
+// makeBlinkyDancer.prototype.step = function () {
+//   makeDancer.prototype.step.call(this);
+// }
--- src/dancer.js
+++ src/dancer.js
@@ -1,30 +1,17 @@
 var makeDancer = function (top, left, timeBetweenSteps) {
   this.$node = $('<span class="dancer"></span>');
-  this.step = function () {
-    setTimeout(this.step, timeBetweenSteps);
-  }
-
-  this.setPosition = function (top, left) {
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    this.$node.css(styleSettings);
-  }
-
-  this.step();
-  this.setPosition(top, left);
-  // this.setPosition(top, left);
+  makeDancer.prototype.step.call(this);
+  this.timeBetweenSteps = timeBetweenSteps
 }
 
+makeDancer.prototype.step = function () {
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+}
 
-// makeDancer.prototype.step = function () {
-//   setTimeout(this.step, this.timeBetweenSteps);
-// };
-// makeDancer.prototype.setPosition = function (top, left) {
-//   var styleSettings = {
-//     top: top,
-//     left: left
-//   };
-//   this.$node.css(styleSettings);
-// };
+makeDancer.prototype.setPosition = function (top, left) {
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
+}
