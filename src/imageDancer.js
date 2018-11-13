846c89ec83839ca7bc1a86cc63f9aebfb7a5024a
mariomui <yi.kan.mui@gmail.com>
2018-11-13 01:42:33 -0800

    Lineup Feature
    Other features pending.

--- src/imageDancer.js
+++ src/imageDancer.js
@@ -1,23 +1,25 @@
-var makeDuckDancer = function (top, left, timeBetweenSteps) {
-  makeDancer.call(this, ...arguments);
-  // this.$node = $('<span class="dancer"></span>');
-  this.setPosition(top, left);
+var imageDancer = function(top, left, timeBetweenSteps) {
+    makeDancer.call(this, ...arguments);
+    // this.$node = $('<span class="dancer"></span>');
+    this.setPosition(top, left);
 }
 
-makeDuckDancer.prototype = Object.create(makeDancer.prototype);
-makeDuckDancer.prototype.constructor = makeDuckDancer;
+imageDancer.prototype = Object.create(makeDancer.prototype);
+imageDancer.prototype.constructor = imageDancer;
+// $('.lineup').appendTo('body');
+imageDancer.prototype.step = function() {
+    // makeDancer.prototype.step.call(this);
 
-makeDuckDancer.prototype.step = function () {
-  // makeDancer.prototype.step.call(this);
-  // this.$node.toggle();
+    this.$node.addClass('imager')
 
-  this.$node.addClass('imager');
-  // $('<img src="./assets/giphy.gif">').appendTo(".imager");
-  // this.$node.css('top', 130);
-  // this.$.animate({ opacity: .35 }, 110000);
-  var jack = Math.random() * 75;
-  var back = Math.random() * 120;
-  console.log(jack, back);
-  // this.setPosition(jack, back);
+    // this.$node.addClass('this');
+    // makeDancer.prototype.lineup.call(this);
+    // $('<img src="./assets/giphy.gif">').appendTo(".imager");
+    // this.$node.css('top', 130);
+    // this.$.animate({ opacity: .35 }, 110000);
+    // var jack = Math.random() * 75;
+    // var back = Math.random() * 120;
+    // console.log(jack, back);
+    // $('.lineup').appendTo('.lineup');
 
 }
--- src/init.js
+++ src/init.js
@@ -1,46 +1,45 @@
-$(document).ready(function () {
-  window.dancers = [];
+$(document).ready(function() {
+    window.dancers = [];
 
-  $('.addDancerButton').on('click', function (event) {
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
+    $('.addDancerButton').on('click', function(event) {
+        /* This function sets up the click handlers for the create-dancer
+         * buttons on dancefloor.html. You should only need to make one small change to it.
+         * As long as the "data-dancer-maker-function-name" attribute of a
+         * class="addDancerButton" DOM node matches one of the names of the
+         * maker functions available in the global scope, clicking that node
+         * will call the function to make the dancer.
+         */
 
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
-    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-    //mario: .data-> Store arbitrary data associated with the specified element and/or return the value that was set.
+        /* dancerMakerFunctionName is a string which must match
+         * one of the dancer maker functions available in global scope.
+         * A new object of the given type will be created and added
+         * to the stage.
+         */
+        var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+        //mario: .data-> Store arbitrary data associated with the specified element and/or return the value that was set.
 
 
 
-    // get the maker function for the kind of dancer we're supposed to make
-    var dancerMakerFunction = window[dancerMakerFunctionName];
+        // get the maker function for the kind of dancer we're supposed to make
+        var dancerMakerFunction = window[dancerMakerFunctionName];
 
-    // make a dancer with a random position
+        // make a dancer with a random position
 
-    var dancer = new dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
-      Math.random() * 1000
-    );
-    window.dancers.push(dancer);
-    $('.lineup').on('click', function () {
-      for (var i = 0; i < window.dancers.length; i++) {
-        //lineup function shoudl be attached to each individual
-        const currentElem = window.dancers[i];
-        currentElem.lineup(i);
-      }
+        var dancer = new dancerMakerFunction(
+            $("body").height() * Math.random(),
+            $("body").width() * Math.random(),
+            Math.random() * 1000
+        );
+        window.dancers.push(dancer);
+        $('.lineup').on('click', function() {
+            for (var i = 0; i < window.dancers.length; i++) {
+                //lineup function shoudl be attached to each individual
+                const currentElem = window.dancers[i];
+                currentElem.lineup(i);
+            }
+        });
+        console.log(dancers);
+        // debugger;
+        $('body').append(dancer.$node);
     });
-    console.log(dancers);
-    // debugger;
-    $('body').append(dancer.$node);
-  });
 });
-
