e2ca7be905bf62841b6bff7c4d040c01462dd9ed
mariomui <yi.kan.mui@gmail.com>
2018-11-24 16:51:15 -0800

    Save Point Before ES6 Update

--- src/dancer.js
+++ src/dancer.js
@@ -1,102 +1,102 @@
-var makeDancer = function (top, left, timeBetweenSteps) {
-  this.$node = $('<span class="dancer"></span>');
-  this.timeBetweenSteps = timeBetweenSteps
-  makeDancer.prototype.step.call(this);
+var makeDancer = function(top, left, timeBetweenSteps) {
+    this.$node = $('<span class="dancer"></span>');
+    this.timeBetweenSteps = timeBetweenSteps
+    makeDancer.prototype.step.call(this);
 }
 
-makeDancer.prototype.step = function () {
-  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+makeDancer.prototype.step = function() {
+    setTimeout(this.step.bind(this), this.timeBetweenSteps);
 }
 
 // Sets random position upon instantiation.
-makeDancer.prototype.setPosition = function (top, left) {
-  var styleSettings = {
-    top: top,
-    left: left
-  };
-  this.$node.css(styleSettings);
+makeDancer.prototype.setPosition = function(top, left) {
+    var styleSettings = {
+        top: top,
+        left: left
+    };
+    this.$node.css(styleSettings);
 }
 
 // Functionality to line all dancers up for a line dance.
-makeDancer.prototype.lineup = function (i) {
-  var styleSettings = {
-    top: 600,
-    left: (document.width / window.dancers.length) * i,
-  };
-  this.$node.css(styleSettings);
+makeDancer.prototype.lineup = function(i) {
+    var styleSettings = {
+        top: 600,
+        left: (document.width / window.dancers.length) * i,
+    };
+    this.$node.css(styleSettings);
 }
 
 // Functionality to line Dancers up to the left
-makeDancer.prototype.leftLineup = function (i) {
-  var styleSettings = {
-    left: (window.innerWidth / 2) - 500,
-    top: (window.innerHeight / window.dancers.length) * i,
+makeDancer.prototype.leftLineup = function(i) {
+    var styleSettings = {
+        left: (window.innerWidth / 2) - 500,
+        top: (window.innerHeight / window.dancers.length) * i,
 
-  }
-  this.$node.css(styleSettings);
+    }
+    this.$node.css(styleSettings);
 }
 
 // Functionality to line OtherDancers up to the right
-makeDancer.prototype.rightLineup = function (i) {
-  var styleSettings = {
-    left: (window.innerWidth / 2),
-    top: (window.innerHeight / window.dancers.length) * i,
+makeDancer.prototype.rightLineup = function(i) {
+    var styleSettings = {
+        left: (window.innerWidth / 2),
+        top: (window.innerHeight / window.dancers.length) * i,
 
-  }
-  this.$node.css(styleSettings);
+    }
+    this.$node.css(styleSettings);
 }
 
-makeDancer.prototype.hover = function () {
-  console.log('fdjhsok');
+makeDancer.prototype.hover = function() {
+    console.log('fdjhsok');
 }
 
 //input oves the this to the object.
-makeDancer.prototype.move = function (otherDancer) {
-  var left = otherDancer.$node.css('left');
-  var top = otherDancer.$node.css('top');
-  var styleSettings = {
-    top: top,
-    left: left,
-    // transition-timing - function: 'ease',
+makeDancer.prototype.move = function(otherDancer) {
+    var left = otherDancer.$node.css('left');
+    var top = otherDancer.$node.css('top');
+    var styleSettings = {
+        top: top,
+        left: left,
+        // transition-timing - function: 'ease',
 
-    // /* Also the same as */
-    // transition-timing - function: 'cubic-bezier(0.25, 0.1, 0.25, 1)',
-  };
-  console.log(this.$node, '!');
-  this.$node.css(styleSettings);
+        // /* Also the same as */
+        // transition-timing - function: 'cubic-bezier(0.25, 0.1, 0.25, 1)',
+    };
+    console.log(this.$node, '!');
+    this.$node.css(styleSettings);
 }
 
 
-makeDancer.prototype.keydown = function (evt) {
-  var topvalue = dancers[0].$node.css('top');
-  var leftvalue = dancers[0].$node.css('left');
-  topvalue = Number(topvalue.split('px')[0]);
-  topvalue += 75;
-  console.log(topvalue);
-  dancers[0].setPosition(topvalue, leftvalue);
+makeDancer.prototype.keydown = function(evt) {
+    var topvalue = dancers[0].$node.css('top');
+    var leftvalue = dancers[0].$node.css('left');
+    topvalue = Number(topvalue.split('px')[0]);
+    topvalue += 75;
+    console.log(topvalue);
+    dancers[0].setPosition(topvalue, leftvalue);
 }
 
-makeDancer.prototype.keyup = function (evt) {
-  var topvalue = dancers[0].$node.css('top');
-  var leftvalue = dancers[0].$node.css('left');
-  topvalue = Number(topvalue.split('px')[0]);
-  topvalue -= 75;
-  console.log(topvalue);
-  dancers[0].setPosition(topvalue);
+makeDancer.prototype.keyup = function(evt) {
+    var topvalue = dancers[0].$node.css('top');
+    var leftvalue = dancers[0].$node.css('left');
+    topvalue = Number(topvalue.split('px')[0]);
+    topvalue -= 75;
+    console.log(topvalue);
+    dancers[0].setPosition(topvalue);
 }
 
-makeDancer.prototype.keyleft = function (evt) {
-  var leftValue = dancers[0].$node.css('left');
-  var topvalue = dancers[0].$node.css('top');
-  leftValue = Number(leftValue.split('px')[0]);
-  leftValue -= 75;
-  dancers[0].setPosition(topvalue, leftValue);
+makeDancer.prototype.keyleft = function(evt) {
+    var leftValue = dancers[0].$node.css('left');
+    var topvalue = dancers[0].$node.css('top');
+    leftValue = Number(leftValue.split('px')[0]);
+    leftValue -= 75;
+    dancers[0].setPosition(topvalue, leftValue);
 }
 
-makeDancer.prototype.keyright = function (evt) {
-  var leftValue = dancers[0].$node.css('left');
-  var topvalue = dancers[0].$node.css('top');
-  leftValue = Number(leftValue.split('px')[0]);
-  leftValue += 75;
-  dancers[0].setPosition(topvalue, leftValue);
+makeDancer.prototype.keyright = function(evt) {
+    var leftValue = dancers[0].$node.css('left');
+    var topvalue = dancers[0].$node.css('top');
+    leftValue = Number(leftValue.split('px')[0]);
+    leftValue += 75;
+    dancers[0].setPosition(topvalue, leftValue);
 }
--- src/init.js
+++ src/init.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
+$(document).ready(function() {
     window.dancers = [];
     window.otherDancers = []; //mario
 
-    $('.addDancerButton').on('click', function (event) {
+    $('.addDancerButton').on('click', function(event) {
         /* This function sets up the click handlers for the create-dancer
          * buttons on dancefloor.html. You should only need to make one small change to it.
          * As long as the "data-dancer-maker-function-name" attribute of a
@@ -40,7 +40,7 @@ $(document).ready(function () {
         }
         //to grow it.
         var flag = true;
-        $('.dancer').on('click', function () {
+        $('.dancer').on('click', function() {
             if (flag) {
 
                 $('.dancer').css('transform', 'scale(1.5)');
@@ -52,7 +52,7 @@ $(document).ready(function () {
 
         })
 
-        $('.lineup').on('click', function () {
+        $('.lineup').on('click', function() {
             for (var i = 0; i < window.dancers.length; i++) {
                 const currentElem = window.dancers[i];
                 currentElem.lineup(i);
@@ -64,19 +64,19 @@ $(document).ready(function () {
             }
 
         });
-        $('.horizLineup').on('click', function () {
-            for (var i = 0; i < window.dancers.length; i++) {
-                const currentElem = window.dancers[i];
-                currentElem.leftLineup(i);
-            }
+        $('.horizLineup').on('click', function() {
+                for (var i = 0; i < window.dancers.length; i++) {
+                    const currentElem = window.dancers[i];
+                    currentElem.leftLineup(i);
+                }
 
-            for (var r = 0; r < window.otherDancers.length; r++) {
-                const currentElem = window.otherDancers[r];
-                currentElem.rightLineup(r);
-            }
-        })
-        //on a click this should move one dancer to another.
-        $('.mover').on('click', function () {
+                for (var r = 0; r < window.otherDancers.length; r++) {
+                    const currentElem = window.otherDancers[r];
+                    currentElem.rightLineup(r);
+                }
+            })
+            //on a click this should move one dancer to another.
+        $('.mover').on('click', function() {
             for (var i = 0; i < window.dancers.length; i++) {
                 const currentElem = window.dancers[i];
                 const nextElem = window.otherDancers[i];
@@ -85,7 +85,7 @@ $(document).ready(function () {
             //dancers[i]$.node.css('left') gets you the left property.
         })
 
-        document.addEventListener('keydown', function (event) {
+        document.addEventListener('keydown', function(event) {
             if (event.code == 'ArrowDown') {
                 makeDancer.prototype.keydown(event);
             } else if (event.code === 'ArrowUp') {
